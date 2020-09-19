d5650f8e9e2169cfe4f4d38f947d651bd14ebcc2
Nishil Shah <nishilshah703@gmail.com>
2020-09-19 13:52:34 +0530

    Updates

---
+++ precache-manifest.a0cd06ddfac3a6872ec9de84274656e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505980144eb0b773889b75ad70289eb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dd37f3ed8fec8973a2cc",
+    "url": "/static/css/main.f60eae6a.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd37f3ed8fec8973a2cc",
+    "url": "/static/js/main.35d3c5ef.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
